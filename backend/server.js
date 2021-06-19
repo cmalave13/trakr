5cbf17af247aaa91de0541f0f064a86fcbeadc1e
Charlie Malave <72099847+cmalave13@users.noreply.github.com>
2021-06-18 22:38:19 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 //const MongoClient = require('mongodb').MongoClient;
 const uri =
-  "mongodb+srv://charlie:mTMZp717YbxJ1aDN@cluster0.zaifw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// ;
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
